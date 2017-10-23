7f5479bc92908f5aad178c8e5f8e6cc9a2428a80
Eric <erg52@pitt.edu>
2017-10-23 15:26:23 -0400

    initial commit

---
+++ src/main/java/Exercise3.java
@@ -0,0 +1,23 @@
+public class Exercise3{
+
+    public static void main(String[] args){
+        int num = parse(args);
+        if(num == -1){
+            System.exit(1);
+        }
+        int tri = Tri(num);
+        int lazy = Lazy(num);
+    }
+
+    public static int Tri(int n){
+        return 0;
+    }
+
+    public static int Lazy(int n){
+        return 0;
+    }
+
+    public static int parse(String[] s){
+        return 0;
+    }
+}
