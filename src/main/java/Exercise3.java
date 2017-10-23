4bd1dbb437c6fe4a03be2eed354cdaf679f20f23
Eric <erg52@pitt.edu>
2017-10-23 15:56:01 -0400

    implemented triN and lazyN

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -5,19 +5,74 @@ public class Exercise3{
         if(num == -1){
             System.exit(1);
         }
-        int tri = Tri(num);
-        int lazy = Lazy(num);
+        int tri = triN(num);
+        int lazy = lazyN(num);
+        if(tri ==  -1 || lazy == 1){
+            System.exit(1);
+        }
+        else{
+            System.out.println("Tri(n) = " + tri);
+            System.out.println("Lazy(n) = " + lazy);
+        }
     }
 
-    public static int Tri(int n){
-        return 0;
+    public static int triN(int n){
+        if(n<0){
+            return -1;
+        }
+        if(n == 0){
+            return 0;
+        }
+        else{
+            int sum = 0;
+            try{
+                for(int i=1; i<=n; i++){
+                    sum += i;
+                }
+                return sum;
+            }
+            catch(Exception ex){
+                return -1;
+            }
+        }
+
     }
 
-    public static int Lazy(int n){
-        return 0;
+    public static int lazyN(int n){
+        if(n<0){
+            return -1;
+        }
+        if(n == 0){
+            return 1;
+        }
+        else{
+            try{
+                int result = (n*n + n + 1)/2;
+                return result;
+            }
+            catch(Exception ex){
+                return -1;
+            }
+        }
     }
 
     public static int parse(String[] s){
-        return 0;
+        if(s.length != 1 || s == null){
+            return -1;
+        }
+        else{
+            try{
+                int temp = Integer.parseInt(s[0]);
+                if(temp < 0){
+                    return -1;
+                }
+                else{
+                    return temp;
+                }
+            }
+            catch(Exception ex){
+                return -1;
+            }
+        }
     }
 }
